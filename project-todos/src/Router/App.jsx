658cc7da54604c694528dc216c7beb8d1ec36707
alexvtl <alexandre.vital@hotmail.fr>
2023-10-17 10:37:47 +0200

    "material ui"

--- project-todos/src/Router/App.jsx
+++ project-todos/src/Router/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "../Components/Nav/nav";
 function App() {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route />
       </Routes>
